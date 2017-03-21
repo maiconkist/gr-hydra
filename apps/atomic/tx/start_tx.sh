3491590c711f5eb5a8239917af6fd435933df348
maiconkist <maiconkist@gmail.com>
2017-03-21 17:40:56 +0000

    update

--- apps/atomic/tx/start_tx.sh
+++ apps/atomic/tx/start_tx.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-./hydra_tx.py --vr1-file ../../vr1fifo --vr2-file ../../vr2fifo --host-ip 127.0.0.1
+
+# works with:
+#     B210, direct cable connection
+python hydra_tx.py --vr1-file ../../vr1fifo --vr2-file ../../vr2fifo --vr1-tx-amplitude 0.2 --vr2-tx-amplitude 0.1 -A J1 --tx-gain 60
