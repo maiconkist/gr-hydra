c35ceb0f82ac0c01bddad539bcdf91aa38a79564
Maicon Kist <maiconkist@gmail.com>
2018-03-22 19:40:00 +0100

    update

--- apps/atomic/tx/start_tx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-
-# works with:
-#     B210, direct cable connection
-python hydra_tx.py --lte-file ../../vr1fifo --nbiot-file ../../vr2fifo --lte-tx-amplitude 0.2 --nbiot-tx-amplitude 0.1 -A J1 --tx-gain 60
--- apps/wishful_control/wishful_simple_controller.py
+++ apps/wishful_control/wishful_simple_controller.py
@@ -39,8 +39,8 @@ nodes = {}
 program_running = None
 the_variables = {}
 
-TOTAL_NODES = 3
-NODE_NAMES = ["tx", "rx1", "rx2"]
+TOTAL_NODES = 2
+NODE_NAMES = ["tx", "lte", "nbiot"]
 
 conf = {
 
@@ -48,20 +48,20 @@ conf = {
     'files' : {
 
     	"tx" :  "/users/kistm/gr-hydra/apps/atomic/tx/vr1_vr2_tx.grc", 
-    	"rx1" : "/users/kistm/gr-hydra/apps/atomic/rx/vr1_rx.grc", 
-    	"rx2" : "/users/kistm/gr-hydra/apps/atomic/rx/vr2_rx.grc", 
+    	"lte" : "/users/kistm/gr-hydra/apps/atomic/rx/lte.grc", 
+    	"nbiot" : "/users/kistm/gr-hydra/apps/atomic/rx/nbiot.grc", 
     },
 
     'program_getters' : {
-        "tx":  ["bandwidth", "center_freq"],
-        "rx1": ["bandwidth", "center_freq", "throughput" ],
-        "rx2": ["bandwidth", "center_freq", "throughput"],
+        "tx":    ["bandwidth", "center_freq"],
+        "lte":   ["bandwidth", "center_freq", "throughput" ],
+        "nbiot": ["bandwidth", "center_freq", "throughput"],
     },
 
     'program_args': {
         "tx":  [""], 
-        "rx1": [""],
-        "rx2": [""],
+        "lte": [""],
+        "nbiot": [""],
     },
 }
 
@@ -77,12 +77,12 @@ def new_node(node):
     #if node.name not in NODE_NAMES:
     #    log.info("Node '%s' is not part of this showcase. Ignoring it" % (node.name, ))
     #else:
-    if node.name in ['tx', 'rx1', 'rx2']:
+    if node.name in ['tx', 'lte', 'nbiot']:
         program_name = node.name
         program_code = open(conf['files'][program_name], "r").read()
         program_args = conf['program_args'][node.name]
 
-        controller.blocking(False).node(node).radio.iface('usrp').activate_radio_program({'program_name': program_name, 'program_code': program_code, 'program_args': program_args,'program_type': 'py'})
+        controller.blocking(False).node(node).radio.iface('usrp').activate_radio_program({'program_name': program_name, 'program_code': program_code, 'program_args': program_args,'program_type': 'grc', 'program_port': 1235})
 
 @controller.node_exit_callback()
 def node_exit(node, reason):
@@ -103,8 +103,7 @@ def default_callback(group, node, cmd, data):
 def get_vars_response(group, node, data):
     log.info("{} get_vars_response : Group:{}, NodeId:{}, msg:{}".format(datetime.datetime.now(), group, node.id, data))
 
-    #if node.name == 'rx1':
-    if node.name == 'rx1':
+    if node.name == 'lte':
         the_variables['rx1_pkt_rcv'] = data['pkt_rcvd'] if 'pkt_rcvd' in data else 'NA'
         the_variables['rx1_pkt_right'] = data['pkt_right'] if 'pkt_right' in data else 'NA'
         the_variables['rx1_center_freq'] = data['center_freq'] if 'center_freq' in data else 'NA'
@@ -114,10 +113,8 @@ def get_vars_response(group, node, data):
            the_variables['rx1_throughput'] = str( float(data['throughput'])/1000.0) + " Kbps"
         else:
            the_variables['rx1_throughput'] = 'NA'
-		
-	 
-    elif node.name == 'rx2':
-    #elif node.name == 'rx2':
+
+    elif node.name == 'nbiot':
         the_variables['rx2_pkt_rcv'] = data['pkt_rcvd'] if 'pkt_rcvd' in data else 'NA'
         the_variables['rx2_pkt_right'] = data['pkt_right'] if 'pkt_right' in data else 'NA'
         the_variables['rx2_center_freq'] = data['center_freq'] if 'center_freq' in data else 'NA'
@@ -151,35 +148,35 @@ def exec_loop():
                 log.info("Requesting data to VR TX")
                 controller.blocking(False).node(nodes['tx']).radio.iface('usrp').get_parameters(conf['program_getters']['tx'])
 
-            if 'rx1' in nodes:
+            if 'lte' in nodes:
                 log.info("Requesting data to VR LTE")
-                controller.blocking(False).node(nodes['rx1']).radio.iface('usrp').get_parameters(conf['program_getters']['rx1'])
+                controller.blocking(False).node(nodes['lte']).radio.iface('usrp').get_parameters(conf['program_getters']['lte'])
 
-            if 'rx2' in nodes:
+            if 'nbiot' in nodes:
                 log.info("Requesting data to VR NB-IoT")
-                controller.blocking(False).node(nodes['rx2']).radio.iface('usrp').get_parameters(conf['program_getters']['rx2'])
+                controller.blocking(False).node(nodes['nbiot']).radio.iface('usrp').get_parameters(conf['program_getters']['nbiot'])
 
 
-            # set variables
-            setters = {}
-            try:
-                setters = pickle.load(open(SETTER_FILE, "rb"))
-                pickle.dump({}, open(SETTER_FILE, "wb"))
-            except Exception as e:
-                log.info("Could not open setters file")
-                log.info(e)
+            ## set variables
+            #setters = {}
+            #try:
+            #    setters = pickle.load(open(SETTER_FILE, "rb"))
+            #    pickle.dump({}, open(SETTER_FILE, "wb"))
+            #except Exception as e:
+            #    log.info("Could not open setters file")
+            #    log.info(e)
 
-            if 'tx' in setters.keys() and 'tx' in nodes:
-                    log.info("Setting configuration of node TX") 
-                    controller.blocking(False).node(nodes['tx']).radio.iface('usrp').set_parameters(setters['tx'])
+            #if 'tx' in setters.keys() and 'tx' in nodes:
+            #        log.info("Setting configuration of node TX") 
+            #        controller.blocking(False).node(nodes['tx']).radio.iface('usrp').set_parameters(setters['tx'])
 
-            if 'rx1' in setters.keys() and 'rx1' in nodes:
-                    log.info("Setting configuration of node RX1") 
-                    controller.blocking(False).node(nodes['rx1']).radio.iface('usrp').set_parameters(setters['rx1'])
+            #if 'lte' in setters.keys() and 'lte' in nodes:
+            #        log.info("Setting configuration of node LTE") 
+            #        controller.blocking(False).node(nodes['lte']).radio.iface('usrp').set_parameters(setters['lte'])
 
-            if 'rx2' in setters.keys() and 'rx2' in nodes:
-                    log.info("Setting configuration of node RX2") 
-                    controller.blocking(False).node(nodes['rx2']).radio.iface('usrp').set_parameters(setters['rx2'])
+            #if 'nbiot' in setters.keys() and 'nbiot' in nodes:
+            #        log.info("Setting configuration of node NB-IoT") 
+            #        controller.blocking(False).node(nodes['nbiot']).radio.iface('usrp').set_parameters(setters['nbiot'])
 
             gevent.sleep(2)
 
