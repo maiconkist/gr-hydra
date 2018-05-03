2363a551edddf54cdc5f5b4a6dbdbad223d01b50
Maicon Kist <maiconkist@gmail.com>
2018-05-03 23:45:12 +0200

    update

--- apps/radios/radio_master.py
+++
@@ -1,7 +0,0 @@
-def build_rx_from_name(name, callback, options = None):
-    exec('import radio_' + name + ' as radio')
-    return radio.build_rx(callback, options)
-
-def build_usrp_rx(name, options = None):
-    exec('import radio_' + name + ' as radio')
-    radio.build_usrp_rx(options)
