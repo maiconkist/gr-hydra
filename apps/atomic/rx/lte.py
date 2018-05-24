577565b0ad1241b3044d313d603f9c75f50c5a9d
Maicon Kist <maiconkist@gmail.com>
2018-05-24 15:43:22 +0100

    update

--- apps/atomic/rx/lte.py
+++ apps/atomic/rx/lte.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Lte
-# Generated: Tue May 22 18:42:25 2018
+# Generated: Thu May 24 15:42:49 2018
 ##################################################
 
 from gnuradio import blocks
@@ -29,6 +29,8 @@ class lte(gr.top_block):
         ##################################################
         # Variables
         ##################################################
+        self.bytes_tx = bytes_tx = 0
+        self.bytes_rx = bytes_rx = 0
         self.zcpu = zcpu = psutil
         self._samprate_config = ConfigParser.ConfigParser()
         self._samprate_config.read('./default')
@@ -42,6 +44,7 @@ class lte(gr.top_block):
         try: freq = self._freq_config.getfloat("usrp_hydra", "txfreq1")
         except: freq = 949.5e6
         self.freq = freq
+        self.error_rate = error_rate = bytes_rx/(bytes_tx if bytes_tx>0 else 1)
         self.cpu_percent = cpu_percent = 0
 
         ##################################################
@@ -49,6 +52,15 @@ class lte(gr.top_block):
         ##################################################
         self.probeiq = blocks.probe_rate(gr.sizeof_gr_complex*1, 500.0, 0.15)
         self.probe1_1 = blocks.probe_rate(gr.sizeof_char*1, 500.0, 0.15)
+        self.digital_ofdm_rx_0 = digital.ofdm_rx(
+        	  fft_len=64, cp_len=16,
+        	  frame_length_tag_key='frame_'+"length",
+        	  packet_length_tag_key="length",
+        	  bps_header=1,
+        	  bps_payload=1,
+        	  debug_log=False,
+        	  scramble_bits=False
+        	 )
         self.xmlrpc_server_0 = SimpleXMLRPCServer.SimpleXMLRPCServer(('localhost', 1235), allow_none=True)
         self.xmlrpc_server_0.register_instance(self)
         self.xmlrpc_server_0_thread = threading.Thread(target=self.xmlrpc_server_0.serve_forever)
@@ -91,15 +103,6 @@ class lte(gr.top_block):
         _rx_goodput_thread.daemon = True
         _rx_goodput_thread.start()
 
-        self.digital_ofdm_rx_0 = digital.ofdm_rx(
-        	  fft_len=64, cp_len=16,
-        	  frame_length_tag_key='frame_'+"length",
-        	  packet_length_tag_key="length",
-        	  bps_header=1,
-        	  bps_payload=1,
-        	  debug_log=False,
-        	  scramble_bits=False
-        	 )
 
         def _cpu_percent_probe():
             while True:
@@ -113,6 +116,32 @@ class lte(gr.top_block):
         _cpu_percent_thread.daemon = True
         _cpu_percent_thread.start()
 
+
+        def _bytes_tx_probe():
+            while True:
+                val = self.digital_ofdm_rx_0.crc.nitems_written(0)
+                try:
+                    self.set_bytes_tx(val)
+                except AttributeError:
+                    pass
+                time.sleep(1.0 / (10))
+        _bytes_tx_thread = threading.Thread(target=_bytes_tx_probe)
+        _bytes_tx_thread.daemon = True
+        _bytes_tx_thread.start()
+
+
+        def _bytes_rx_probe():
+            while True:
+                val = self.digital_ofdm_rx_0.crc.nitems_read(0)
+                try:
+                    self.set_bytes_rx(val)
+                except AttributeError:
+                    pass
+                time.sleep(1.0 / (10))
+        _bytes_rx_thread = threading.Thread(target=_bytes_rx_probe)
+        _bytes_rx_thread.daemon = True
+        _bytes_rx_thread.start()
+
         self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, "Rx'd Packet", ""); self.blocks_tag_debug_0.set_display(True)
 
         ##################################################
@@ -122,6 +151,20 @@ class lte(gr.top_block):
         self.connect((self.digital_ofdm_rx_0, 0), (self.probe1_1, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.digital_ofdm_rx_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.probeiq, 0))
+
+    def get_bytes_tx(self):
+        return self.bytes_tx
+
+    def set_bytes_tx(self, bytes_tx):
+        self.bytes_tx = bytes_tx
+        self.set_error_rate(self.bytes_rx/(self.bytes_tx if self.bytes_tx>0 else 1))
+
+    def get_bytes_rx(self):
+        return self.bytes_rx
+
+    def set_bytes_rx(self, bytes_rx):
+        self.bytes_rx = bytes_rx
+        self.set_error_rate(self.bytes_rx/(self.bytes_tx if self.bytes_tx>0 else 1))
 
     def get_zcpu(self):
         return self.zcpu
@@ -154,6 +197,12 @@ class lte(gr.top_block):
     def set_freq(self, freq):
         self.freq = freq
         self.uhd_usrp_source_0.set_center_freq(self.freq, 0)
+
+    def get_error_rate(self):
+        return self.error_rate
+
+    def set_error_rate(self, error_rate):
+        self.error_rate = error_rate
 
     def get_cpu_percent(self):
         return self.cpu_percent
