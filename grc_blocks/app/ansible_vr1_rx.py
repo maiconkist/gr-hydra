606755c35678866301b88ef9444a2a979f62fe99
Maicon Kist <maiconkist@gmail.com>
2018-12-03 18:55:28 +0000

    update ansible rx files

--- grc_blocks/app/ansible_vr1_rx.py
+++ grc_blocks/app/ansible_vr1_rx.py
@@ -3,25 +3,67 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Ansible Vr1 Rx
-# Generated: Mon Dec  3 15:33:23 2018
+# Generated: Mon Dec  3 18:50:26 2018
 ##################################################
 
+from distutils.version import StrictVersion
 
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt5 import Qt
+from PyQt5 import Qt, QtCore
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
+from gnuradio import qtgui
 from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
+import sip
+import sys
 import time
+from gnuradio import qtgui
 
 
-class ansible_vr1_rx(gr.top_block):
+class ansible_vr1_rx(gr.top_block, Qt.QWidget):
 
-    def __init__(self, freqrx=1.2e9, freqtx=1.1e9, samp_rate=200e3, vr1offset=-100e3, vr2offset=700e3):
+    def __init__(self, freqrx=1.2e9, freqtx=1.1e9, samp_rate=200e3, vr1offset=-200e3, vr2offset=700e3):
         gr.top_block.__init__(self, "Ansible Vr1 Rx")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Ansible Vr1 Rx")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "ansible_vr1_rx")
+
+        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+            self.restoreGeometry(self.settings.value("geometry").toByteArray())
+        else:
+            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
         ##################################################
         # Parameters
@@ -43,9 +85,45 @@ class ansible_vr1_rx(gr.top_block):
         	),
         )
         self.uhd_usrp_source_0.set_samp_rate(samp_rate*2)
-        self.uhd_usrp_source_0.set_center_freq(freqrx + vr1offset, 0)
+        self.uhd_usrp_source_0.set_center_freq(freqtx + vr1offset, 0)
         self.uhd_usrp_source_0.set_normalized_gain(0, 0)
         self.uhd_usrp_source_0.set_antenna('RX2', 0)
+        self.qtgui_waterfall_sink_x_2 = qtgui.waterfall_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+                1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_2.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_2.enable_grid(False)
+        self.qtgui_waterfall_sink_x_2.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_waterfall_sink_x_2.disable_legend()
+
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_waterfall_sink_x_2.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_2.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_2.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_2.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_2.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_2.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_2_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_2.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_2_win)
         self.digital_ofdm_rx_0 = digital.ofdm_rx(
         	  fft_len=64, cp_len=16,
         	  frame_length_tag_key='frame_'+"len",
@@ -64,19 +142,25 @@ class ansible_vr1_rx(gr.top_block):
         self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_message_debug_0, 'print_pdu'))
         self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tagged_stream_to_pdu_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.digital_ofdm_rx_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_waterfall_sink_x_2, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "ansible_vr1_rx")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
 
     def get_freqrx(self):
         return self.freqrx
 
     def set_freqrx(self, freqrx):
         self.freqrx = freqrx
-        self.uhd_usrp_source_0.set_center_freq(self.freqrx + self.vr1offset, 0)
 
     def get_freqtx(self):
         return self.freqtx
 
     def set_freqtx(self, freqtx):
         self.freqtx = freqtx
+        self.uhd_usrp_source_0.set_center_freq(self.freqtx + self.vr1offset, 0)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -84,13 +168,14 @@ class ansible_vr1_rx(gr.top_block):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate*2)
+        self.qtgui_waterfall_sink_x_2.set_frequency_range(0, self.samp_rate)
 
     def get_vr1offset(self):
         return self.vr1offset
 
     def set_vr1offset(self, vr1offset):
         self.vr1offset = vr1offset
-        self.uhd_usrp_source_0.set_center_freq(self.freqrx + self.vr1offset, 0)
+        self.uhd_usrp_source_0.set_center_freq(self.freqtx + self.vr1offset, 0)
 
     def get_vr2offset(self):
         return self.vr2offset
@@ -108,9 +193,20 @@ def main(top_block_cls=ansible_vr1_rx, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
     tb = top_block_cls()
     tb.start()
-    tb.wait()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
 
 
 if __name__ == '__main__':
