986fc3b11d41ea0fa200371c93dc16537f7568b3
Maicon Kist <maiconkist@gmail.com>
2019-02-22 15:11:39 +0000

    update

--- grc_blocks/app/ansible_hydra_gr_client_2tx_2rx.py
+++ grc_blocks/app/ansible_hydra_gr_client_2tx_2rx.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Ansible Hydra Gr Client 2Tx 2Rx
-# Generated: Thu Feb 14 18:33:34 2019
+# Generated: Fri Feb 22 15:10:56 2019
 ##################################################
 
 
@@ -20,30 +20,31 @@ import threading
 
 class ansible_hydra_gr_client_2tx_2rx(gr.top_block):
 
-    def __init__(self, freqrx=1.1e9+5e6, freqtx=1.1e9, hydraClient='192.168.5.77', samp_rate=200e3, vr1offset=-300e3, vr2offset=700e3):
+    def __init__(self, ansibleIP='192.168.5.78', freqrx=2.1e9+5e6, freqtx=2.1e9, samp_rate=200e3, vr1offset=-300e3, vr2offset=700e3, mul=0.06):
         gr.top_block.__init__(self, "Ansible Hydra Gr Client 2Tx 2Rx")
 
         ##################################################
         # Parameters
         ##################################################
+        self.ansibleIP = ansibleIP
         self.freqrx = freqrx
         self.freqtx = freqtx
-        self.hydraClient = hydraClient
         self.samp_rate = samp_rate
         self.vr1offset = vr1offset
         self.vr2offset = vr2offset
+        self.mul = mul
 
         ##################################################
         # Blocks
         ##################################################
-        self.hydra_gr_sink_0_0 = hydra.hydra_gr_client_sink(2, 'ansibleIP', 5000)
+        self.hydra_gr_sink_0_0 = hydra.hydra_gr_client_sink(2, ansibleIP, 5000)
         self.hydra_gr_sink_0_0.start_client(freqtx + vr2offset, samp_rate, 1024)
-        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(1, 'ansibleIP', 5000)
+        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(1, ansibleIP, 5000)
         self.hydra_gr_sink_0.start_client(freqtx + vr1offset, samp_rate * 2, 1024)
-        self.hydra_gr__source_0_0_0 = hydra.hydra_gr_client_source(2, 'ansibleIP', 'ansibleIP', 5000)
+        self.hydra_gr__source_0_0_0 = hydra.hydra_gr_client_source(2, ansibleIP, ansibleIP, 5000)
         self.hydra_gr__source_0_0_0.start_client(freqrx + vr2offset, samp_rate, 10000)
 
-        self.hydra_gr__source_0_0 = hydra.hydra_gr_client_source(1, 'ansibleIP', 'ansibleIP', 5000)
+        self.hydra_gr__source_0_0 = hydra.hydra_gr_client_source(1, ansibleIP, ansibleIP, 5000)
         self.hydra_gr__source_0_0.start_client(freqrx + vr1offset, samp_rate * 2, 10000)
 
         self.digital_ofdm_tx_0_0 = digital.ofdm_tx(
@@ -86,17 +87,21 @@ class ansible_hydra_gr_client_2tx_2rx(gr.top_block):
         self.blocks_tuntap_pdu_1 = blocks.tuntap_pdu('tap0', 10000, False)
         self.blocks_tagged_stream_to_pdu_0_0 = blocks.tagged_stream_to_pdu(blocks.byte_t, "len")
         self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(blocks.byte_t, "len")
-        self.blocks_tag_debug_0_0 = blocks.tag_debug(gr.sizeof_char*1, 'VR2 RX', ""); self.blocks_tag_debug_0_0.set_display(True)
-        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, 'VR1 RX', ""); self.blocks_tag_debug_0.set_display(True)
+        self.blocks_tag_debug_0_0 = blocks.tag_debug(gr.sizeof_gr_complex*1, 'VR2 RX', ""); self.blocks_tag_debug_0_0.set_display(True)
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_gr_complex*1, 'VR1 RX', ""); self.blocks_tag_debug_0.set_display(True)
         self.blocks_pdu_to_tagged_stream_0_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, "len")
         self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, "len")
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vcc((0.06, ))
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.06, ))
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vcc((mul, ))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((mul, ))
+        self.blocks_message_debug_0_0 = blocks.message_debug()
+        self.blocks_message_debug_0 = blocks.message_debug()
 
         ##################################################
         # Connections
         ##################################################
+        self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_message_debug_0_0, 'print_pdu'))
         self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_tuntap_pdu_1, 'pdus'))
+        self.msg_connect((self.blocks_tagged_stream_to_pdu_0_0, 'pdus'), (self.blocks_message_debug_0, 'print_pdu'))
         self.msg_connect((self.blocks_tagged_stream_to_pdu_0_0, 'pdus'), (self.blocks_tuntap_pdu_1_0, 'pdus'))
         self.msg_connect((self.blocks_tuntap_pdu_1, 'pdus'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
         self.msg_connect((self.blocks_tuntap_pdu_1_0, 'pdus'), (self.blocks_pdu_to_tagged_stream_0_0, 'pdus'))
@@ -104,14 +109,20 @@ class ansible_hydra_gr_client_2tx_2rx(gr.top_block):
         self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.hydra_gr_sink_0_0, 0))
         self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.digital_ofdm_tx_0, 0))
         self.connect((self.blocks_pdu_to_tagged_stream_0_0, 0), (self.digital_ofdm_tx_0_0, 0))
-        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tag_debug_0, 0))
         self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tagged_stream_to_pdu_0, 0))
-        self.connect((self.digital_ofdm_rx_0_0, 0), (self.blocks_tag_debug_0_0, 0))
         self.connect((self.digital_ofdm_rx_0_0, 0), (self.blocks_tagged_stream_to_pdu_0_0, 0))
         self.connect((self.digital_ofdm_tx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.digital_ofdm_tx_0, 0), (self.blocks_tag_debug_0, 0))
         self.connect((self.digital_ofdm_tx_0_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.digital_ofdm_tx_0_0, 0), (self.blocks_tag_debug_0_0, 0))
         self.connect((self.hydra_gr__source_0_0, 0), (self.digital_ofdm_rx_0, 0))
         self.connect((self.hydra_gr__source_0_0_0, 0), (self.digital_ofdm_rx_0_0, 0))
+
+    def get_ansibleIP(self):
+        return self.ansibleIP
+
+    def set_ansibleIP(self, ansibleIP):
+        self.ansibleIP = ansibleIP
 
     def get_freqrx(self):
         return self.freqrx
@@ -124,12 +135,6 @@ class ansible_hydra_gr_client_2tx_2rx(gr.top_block):
 
     def set_freqtx(self, freqtx):
         self.freqtx = freqtx
-
-    def get_hydraClient(self):
-        return self.hydraClient
-
-    def set_hydraClient(self, hydraClient):
-        self.hydraClient = hydraClient
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -149,12 +154,20 @@ class ansible_hydra_gr_client_2tx_2rx(gr.top_block):
     def set_vr2offset(self, vr2offset):
         self.vr2offset = vr2offset
 
+    def get_mul(self):
+        return self.mul
+
+    def set_mul(self, mul):
+        self.mul = mul
+        self.blocks_multiply_const_vxx_0_0.set_k((self.mul, ))
+        self.blocks_multiply_const_vxx_0.set_k((self.mul, ))
+
 
 def argument_parser():
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     parser.add_option(
-        "", "--hydraClient", dest="hydraClient", type="string", default='192.168.5.77',
-        help="Set hydraClient [default=%default]")
+        "", "--ansibleIP", dest="ansibleIP", type="string", default='192.168.5.78',
+        help="Set ansibleIP [default=%default]")
     return parser
 
 
@@ -162,7 +175,7 @@ def main(top_block_cls=ansible_hydra_gr_client_2tx_2rx, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    tb = top_block_cls(hydraClient=options.hydraClient)
+    tb = top_block_cls(ansibleIP=options.ansibleIP)
     tb.start()
     tb.wait()
 
