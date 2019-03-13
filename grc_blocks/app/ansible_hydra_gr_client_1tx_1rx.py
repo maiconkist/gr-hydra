08a49855de658528589cfbd5b853d38fa87f659b
Maicon Kist <maiconkist@gmail.com>
2019-03-13 01:06:30 +0000

    update marathon

--- grc_blocks/app/ansible_hydra_gr_client_1tx_1rx.py
+++ grc_blocks/app/ansible_hydra_gr_client_1tx_1rx.py
@@ -3,42 +3,129 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Ansible Hydra Gr Client 1Tx 1Rx
-# Generated: Tue Mar 12 19:08:30 2019
+# Generated: Wed Mar 13 00:39:11 2019
 ##################################################
 
+from distutils.version import StrictVersion
 
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt5 import Qt
+from PyQt5 import Qt, QtCore
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
+from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import hydra
+import sip
+import sys
 import threading
+from gnuradio import qtgui
 
 
-class ansible_hydra_gr_client_1tx_1rx(gr.top_block):
+class ansible_hydra_gr_client_1tx_1rx(gr.top_block, Qt.QWidget):
 
-    def __init__(self, freqrx=1.1e9 + 5e6, freqtx=1.1e9, hydraClient='192.168.5.241', samp_rate=200e3, vr1offset=-300e3, vr2offset=700e3):
+    def __init__(self, ansibleIP='192.168.5.241', freqrx=1.42e9 + 3e6, freqtx=1.42e9, samp_rate=200e3, vr1offset=-300e3, vr2offset=700e3):
         gr.top_block.__init__(self, "Ansible Hydra Gr Client 1Tx 1Rx")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Ansible Hydra Gr Client 1Tx 1Rx")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "ansible_hydra_gr_client_1tx_1rx")
+
+        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+            self.restoreGeometry(self.settings.value("geometry").toByteArray())
+        else:
+            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
         ##################################################
         # Parameters
         ##################################################
+        self.ansibleIP = ansibleIP
         self.freqrx = freqrx
         self.freqtx = freqtx
-        self.hydraClient = hydraClient
         self.samp_rate = samp_rate
         self.vr1offset = vr1offset
         self.vr2offset = vr2offset
 
         ##################################################
+        # Variables
+        ##################################################
+        self.mul = mul = 0.01
+
+        ##################################################
         # Blocks
         ##################################################
-        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(1, 'ansibleIP', 5000)
+        self._mul_range = Range(0, 1, 0.01, 0.01, 200)
+        self._mul_win = RangeWidget(self._mul_range, self.set_mul, 'mul', "counter_slider", float)
+        self.top_layout.addWidget(self._mul_win)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+                1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_waterfall_sink_x_0.disable_legend()
+
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(1, ansibleIP, 5000)
         self.hydra_gr_sink_0.start_client(freqtx + vr1offset, samp_rate * 2, 1024)
-        self.hydra_gr__source_0_0 = hydra.hydra_gr_client_source(1, 'ansibleIP', 'ansibleIP', 5000)
+        self.hydra_gr__source_0_0 = hydra.hydra_gr_client_source(1, ansibleIP, ansibleIP, 5000)
         self.hydra_gr__source_0_0.start_client(freqrx + vr1offset, samp_rate * 2, 10000)
 
         self.digital_ofdm_tx_0 = digital.ofdm_tx(
@@ -59,11 +146,12 @@ class ansible_hydra_gr_client_1tx_1rx(gr.top_block):
         	  debug_log=False,
         	  scramble_bits=False
         	 )
-        self.blocks_tuntap_pdu_1 = blocks.tuntap_pdu('tap0', 1000, False)
+        self.blocks_tuntap_pdu_1 = blocks.tuntap_pdu('tap0', 1500, False)
         (self.blocks_tuntap_pdu_1).set_max_output_buffer(100000)
         self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(blocks.byte_t, "len")
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, 'VR1 RX', ""); self.blocks_tag_debug_0.set_display(True)
         self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, "len")
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.06, ))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((mul, ))
 
         ##################################################
         # Connections
@@ -72,9 +160,22 @@ class ansible_hydra_gr_client_1tx_1rx(gr.top_block):
         self.msg_connect((self.blocks_tuntap_pdu_1, 'pdus'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.hydra_gr_sink_0, 0))
         self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.digital_ofdm_tx_0, 0))
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tag_debug_0, 0))
         self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tagged_stream_to_pdu_0, 0))
         self.connect((self.digital_ofdm_tx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.hydra_gr__source_0_0, 0), (self.digital_ofdm_rx_0, 0))
+        self.connect((self.hydra_gr__source_0_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "ansible_hydra_gr_client_1tx_1rx")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_ansibleIP(self):
+        return self.ansibleIP
+
+    def set_ansibleIP(self, ansibleIP):
+        self.ansibleIP = ansibleIP
 
     def get_freqrx(self):
         return self.freqrx
@@ -88,17 +189,12 @@ class ansible_hydra_gr_client_1tx_1rx(gr.top_block):
     def set_freqtx(self, freqtx):
         self.freqtx = freqtx
 
-    def get_hydraClient(self):
-        return self.hydraClient
-
-    def set_hydraClient(self, hydraClient):
-        self.hydraClient = hydraClient
-
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
 
     def get_vr1offset(self):
         return self.vr1offset
@@ -112,12 +208,19 @@ class ansible_hydra_gr_client_1tx_1rx(gr.top_block):
     def set_vr2offset(self, vr2offset):
         self.vr2offset = vr2offset
 
+    def get_mul(self):
+        return self.mul
+
+    def set_mul(self, mul):
+        self.mul = mul
+        self.blocks_multiply_const_vxx_0.set_k((self.mul, ))
+
 
 def argument_parser():
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     parser.add_option(
-        "", "--hydraClient", dest="hydraClient", type="string", default='192.168.5.241',
-        help="Set hydraClient [default=%default]")
+        "", "--ansibleIP", dest="ansibleIP", type="string", default='192.168.5.241',
+        help="Set ansibleIP [default=%default]")
     return parser
 
 
@@ -125,9 +228,20 @@ def main(top_block_cls=ansible_hydra_gr_client_1tx_1rx, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    tb = top_block_cls(hydraClient=options.hydraClient)
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(ansibleIP=options.ansibleIP)
     tb.start()
-    tb.wait()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
 
 
 if __name__ == '__main__':
