567840d827ea7e4fa83650ee5957e165772414ce
Maicon Kist <maiconkist@gmail.com>
2019-03-13 01:19:21 +0000

    removed qt qui

--- grc_blocks/app/ansible_hydra_vr1_rx.py
+++ grc_blocks/app/ansible_hydra_vr1_rx.py
@@ -3,22 +3,10 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Ansible Hydra Vr1 Rx
-# Generated: Wed Mar 13 00:35:25 2019
+# Generated: Wed Mar 13 01:18:31 2019
 ##################################################
 
-from distutils.version import StrictVersion
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt5 import Qt, QtCore
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
@@ -26,67 +14,29 @@ from gnuradio import gr
 from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
-import sys
 import time
-from gnuradio import qtgui
 
 
-class ansible_hydra_vr1_rx(gr.top_block, Qt.QWidget):
+class ansible_hydra_vr1_rx(gr.top_block):
 
-    def __init__(self, freqrx=1.42e9, freqtx=1.42e9+3e6, samp_rate=200e3, vr1offset=-300e3, vr2offset=700e3):
+    def __init__(self, freqrx=1.42e9, freqtx=1.42e9+3e6, gain=0.7, mul=0.04, samp_rate=200e3, vr1offset=-300e3, vr2offset=700e3):
         gr.top_block.__init__(self, "Ansible Hydra Vr1 Rx")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Ansible Hydra Vr1 Rx")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "ansible_hydra_vr1_rx")
-
-        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-            self.restoreGeometry(self.settings.value("geometry").toByteArray())
-        else:
-            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
         ##################################################
         # Parameters
         ##################################################
         self.freqrx = freqrx
         self.freqtx = freqtx
+        self.gain = gain
+        self.mul = mul
         self.samp_rate = samp_rate
         self.vr1offset = vr1offset
         self.vr2offset = vr2offset
 
         ##################################################
-        # Variables
-        ##################################################
-        self.mul = mul = 0.04
-        self.gain = gain = 0.7
-
-        ##################################################
         # Blocks
         ##################################################
-        self._mul_range = Range(0, 1, 0.01, 0.04, 200)
-        self._mul_win = RangeWidget(self._mul_range, self.set_mul, 'mul', "counter_slider", float)
-        self.top_layout.addWidget(self._mul_win)
-        self._gain_range = Range(0, 1, 0.01, 0.7, 200)
-        self._gain_win = RangeWidget(self._gain_range, self.set_gain, 'gain', "counter_slider", float)
-        self.top_layout.addWidget(self._gain_win)
         self.uhd_usrp_source_0 = uhd.usrp_source(
         	",".join(("", "")),
         	uhd.stream_args(
@@ -145,11 +95,6 @@ class ansible_hydra_vr1_rx(gr.top_block, Qt.QWidget):
         self.connect((self.digital_ofdm_tx_0_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.digital_ofdm_rx_0_0, 0))
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "ansible_hydra_vr1_rx")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
-
     def get_freqrx(self):
         return self.freqrx
 
@@ -163,6 +108,21 @@ class ansible_hydra_vr1_rx(gr.top_block, Qt.QWidget):
     def set_freqtx(self, freqtx):
         self.freqtx = freqtx
         self.uhd_usrp_sink_0.set_center_freq(self.freqtx + self.vr1offset, 0)
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.uhd_usrp_sink_0.set_normalized_gain(self.gain, 0)
+
+
+    def get_mul(self):
+        return self.mul
+
+    def set_mul(self, mul):
+        self.mul = mul
+        self.blocks_multiply_const_vxx_0_0.set_k((self.mul, ))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -186,21 +146,6 @@ class ansible_hydra_vr1_rx(gr.top_block, Qt.QWidget):
     def set_vr2offset(self, vr2offset):
         self.vr2offset = vr2offset
 
-    def get_mul(self):
-        return self.mul
-
-    def set_mul(self, mul):
-        self.mul = mul
-        self.blocks_multiply_const_vxx_0_0.set_k((self.mul, ))
-
-    def get_gain(self):
-        return self.gain
-
-    def set_gain(self, gain):
-        self.gain = gain
-        self.uhd_usrp_sink_0.set_normalized_gain(self.gain, 0)
-
-
 
 def argument_parser():
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
@@ -211,20 +156,9 @@ def main(top_block_cls=ansible_hydra_vr1_rx, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
     tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.aboutToQuit.connect(quitting)
-    qapp.exec_()
+    tb.wait()
 
 
 if __name__ == '__main__':
