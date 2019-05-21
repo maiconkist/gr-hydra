026232a4dfbfe61d30a3bcdfe0b03ff4c06cfac9
Maicon Kist <maiconkist@gmail.com>
2019-05-21 14:54:15 +0100

    update server files

--- grc_blocks/app/ansible_hydra_gr_server.py
+++ grc_blocks/app/ansible_hydra_gr_server.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Ansible Hydra Gr Server
-# Generated: Wed Apr  3 15:37:03 2019
+# Generated: Tue May 21 13:35:02 2019
 ##################################################
 
 
@@ -18,7 +18,7 @@ import threading
 
 class ansible_hydra_gr_server(gr.top_block):
 
-    def __init__(self, ansibleIPPort='192.168.5.54:5000', freqrx=2.43e9+3e6, freqtx=2.43e9):
+    def __init__(self, ansibleIPPort='192.168.5.234:5000', freqrx=2.43e9+3e6, freqtx=2.43e9):
         gr.top_block.__init__(self, "Ansible Hydra Gr Server")
 
         ##################################################
@@ -62,7 +62,7 @@ class ansible_hydra_gr_server(gr.top_block):
 def argument_parser():
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     parser.add_option(
-        "", "--ansibleIPPort", dest="ansibleIPPort", type="string", default='192.168.5.54:5000',
+        "", "--ansibleIPPort", dest="ansibleIPPort", type="string", default='192.168.5.234:5000',
         help="Set ansibleIPPort [default=%default]")
     parser.add_option(
         "", "--freqrx", dest="freqrx", type="eng_float", default=eng_notation.num_to_str(2.43e9+3e6),
